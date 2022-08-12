f62bf5fab4e1d2e2cd0448fc68d3554897fb5400
Ashok kumar <102199711+Ashok8210@users.noreply.github.com>
2022-08-13 02:42:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+
+
+
+function handleModal(id) {
+    if (document.getElementById(id).style.display == '') {
+        document.getElementById(id).style.display = 'block'
+    }
+    else {
+        document.getElementById(id).style.display = ''
+    }
+
+}
+
+
+
+
+
+// const popup_element = document.getElementById("forget_popup")
+// function show_popup() {
+//     popup_element.style.display = "block";
+// }
+
+// function hide_popup() {
+//     popup_element.style.display = "none";
+// }
+
+
+// <button type="button" class="btn-dark btn" onclick="show_popup()">Forget Password</button>
+
+
+// <div class="popup " id="forget_popup" onclick="hide_popup()">
+//                         <h3>Reset password</h3>
+//                         <p>Recover your password</p>
+//                         <input type="password" placeholder="Recover password">
+//                         <button>Send my password</button>
+//                         <p>Password will dend your email</p>
+
+//                     </div>
+
+
+
